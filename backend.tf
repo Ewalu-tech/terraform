e6e954fda2878d3e4d7452bd3dc5e5a5ba31d735
Ewalu-tech <79178460+Ewalu-tech@users.noreply.github.com>
2022-08-04 21:10:30 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@
 #    backend "s3" {
 #     bucket = "avinash-terraform-state"
 #     key    = "state.tfstate"
-#     region = "us-west-2"
+#     region = "us-east-2"
 #     profile = "jespo"
-# dynamodb_table= "firstprojecttable"
+# dynamodb_table= "testdynamoDBtable"
 #    }
 #  }
 
